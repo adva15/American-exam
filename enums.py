04fe003e92a01f73f211aac1c717d52b008d602b
adva15 <aadva5319@gmail.com>
2025-07-25 13:44:37 +0300

    Add files via upload

---
+++ enums.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+class CardSuit(Enum):
+    HEARTS = 1
+    DIAMONDS = 2
+    CLUBS = 3
+    SPADES = 4
+
+class CardRank(Enum):
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+    SIX = 6
+    SEVEN = 7
+    EIGHT = 8
+    NINE = 9
+    TEN = 10
+    JACK = 11
+    QUEEN = 12
+    KING = 13
+    ACE = 14
+
